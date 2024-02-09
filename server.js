9f7f1ce3bf6994cd05f3b0317c95d522c9527e55
Nehaa1210 <121329987+Nehaa1210@users.noreply.github.com>
2024-02-09 12:28:06 +0530

    Added

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const {connection}=require("./db");
+const { userRouter } = require('./Routes/Userroutes');
+const {postRouter} = require("./Routes/Postroutes")
+
+
+const app = express();
+app.use(express.json())
+app.use("/users",userRouter)
+app.use("/posts",postRouter)
+
+
+app.listen(8080,async() => {
+  // console.log(`Server is running on port ${8080}`);
+ try {
+  await connection
+  console.log("connected db")
+   console.log(`Server is running on port ${8080}`);
+ } catch (error) {
+  console.log(error)
+ }
+});
